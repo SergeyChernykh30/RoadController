9b424a837d2f5b5675d6dc26e586822ca62404cf
Sergey <sergey.chernykh.89@icloud.com>
2022-10-31 21:59:37 +0500

    1. Rename method "toString" to "getCarTitle" 2. Change parammetres of class Car from "public" to "private"

--- src/core/Camera.java
+++ src/core/Camera.java
@@ -7,11 +7,11 @@ public class Camera {
         double randomWeight = 600 + 10000 * Math.random();
 
         Car car = new Car();
-        car.number = randomNumber;
-        car.height = randomHeight;
-        car.weight = randomWeight;
-        car.hasVehicle = Math.random() > 0.5;
-        car.isSpecial = Math.random() < 0.15;
+        car.setNumber(randomNumber);
+        car.setHeight(randomHeight);
+        car.setWeight(randomWeight);
+        car.setVehicle(Math.random() > 0.5);
+        car.setSpecial(Math.random() < 0.15);
 
         return car;
     }
